5ceb3f5ef5d36ca6b2c77c590a33b956170d895a
주영빈 <tonty1222@naver.com>
2022-07-22 16:18:44 +0900

    SSGTUDY 기본 개발환경 설정

--- match/urls.py
+++ match/urls.py
@@ -4,4 +4,6 @@ from match import views
 urlpatterns = [
     path('', views.match, name='match'),
     path('study_detail/', views.study_detail, name='study_detail'),
+    path('study_edit/', views.study_edit, name='study_edit'),
+    path('study_delete', views.study_delete, name='study_delete'),
 ]
--- match/views.py
+++ match/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render
 def match(request):
     return render(request, 'match.html')
 
+# study_edit.html
+def study_edit(request):
+    return render(request, 'study_edit.html')
+
+# study_delete.html
+def study_delete(request):
+    return render(request, 'study_delete.html')
+
+# study_detail.html
 def study_detail(request):
     return render(request, 'study_detail.html')
